30315b49e2b3e7ec19da7a9b95ccaf7ed2e96e60
Rodrigo Yamagushi <rodrigo.yamagushi@stormgroup.com.br>
2021-08-07 16:09:03 -0300

    add mongoose and checking if connection is ok

--- app.js
+++ app.js
@@ -1,5 +1,33 @@
+require('dotenv/config');
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const app = express();
+
+const db_url = `mongodb+srv://teddiur:${process.env.MONGODB_PASSWORD}@cluster0.lge5r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const options = {
+  reconnectTries: Number.MAX_VALUE,
+  reconnectInterval: 500,
+  useNewUrlParser: true,
+};
+
+mongoose.connect(db_url, options);
+mongoose.set('useCreateIndex', true);
+
+mongoose.connection.on('error', err => {
+  console.log(`erro na conexão com o banco de dados ${err}`);
+});
+
+mongoose.connection.on('disconnected', () => {
+  console.log(`Aplicação desconectada do banco de dados`);
+});
+
+mongoose.connection.on('connected', () => {
+  console.log(`Aplicação conectada ao banco de dados`);
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const indexRoute = require('./routes/index');
 const usersRoute = require('./routes/users');
