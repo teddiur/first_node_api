7742341d829042efe7788ac7c933796933f5724c
Rodrigo Yamagushi <rodrigo.yamagushi@stormgroup.com.br>
2021-08-08 18:30:39 -0300

    add basic encryption and auth (it's pretty dummy)

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Users = require('../models/users');
+const bcrypt = require('bcrypt');
 
 router.get('/', (req, res) => {
   Users.find({}, (err, data) => {
@@ -14,17 +15,35 @@ router.post('/create', (req, res) => {
 
   if (!email || !password) return res.send({ error: 'Dados insuficientes' });
 
-  Users.findOne({ email }, (error, data) => {
-    if (error) return res.send({ error: 'Erro ao buscar o usuário' });
+  Users.findOne({ email }, (err, data) => {
+    if (err) return res.send({ error: 'Erro ao buscar o usuário' });
     if (data) return res.send({ error: 'Usuário já cadastrado' });
 
-    Users.create({ email, password }, (error, data) => {
-      if (error) return res.send({ error: `Erro ao criar o usuário ${error}` });
+    Users.create({ email, password }, (err, data) => {
+      if (err) return res.send({ error: `Erro ao criar o usuário ${err}` });
 
       data.password = undefined;
       return res.send(data);
     });
   });
+});
+
+router.post('/auth', (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) return res.send({ error: 'Dados insuficientes' });
+
+  Users.findOne({ email }, (err, data) => {
+    if (err) return res.send({ error: 'Erro ao buscar o usuário!' });
+    if (!data) return res.send({ error: 'Usuário não registrado!' });
+
+    bcrypt.compare(password, data.password, (err, same) => {
+      if (!same) return res.send({ error: 'Erro ao autenticar o usuário' });
+
+      data.password = undefined;
+      return res.send(data);
+    });
+  }).select('+password');
 });
 
 module.exports = router;
