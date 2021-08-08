34d2c9bb2109fe39254dbe4d64ff73a673524779
Rodrigo Yamagushi <rodrigo.yamagushi@stormgroup.com.br>
2021-08-08 17:48:35 -0300

    implement user get/post

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const Users = require('../models/users');
 
 router.get('/', (req, res) => {
-  const obj = req.query;
-  return res.send({ message: `Usuário ${obj.name} tem ${obj.yearsOld} anos` });
+  Users.find({}, (err, data) => {
+    if (err) return res.send({ error: `Erro na consulta de usuários.` });
+    return res.send(data);
+  });
 });
 
 router.post('/create', (req, res) => {
-  const obj = req.query;
-  return res.send({ message: `Usuário cadastrado` });
+  const { email, password } = req.body;
+
+  if (!email || !password) return res.send({ error: 'Dados insuficientes' });
+
+  Users.findOne({ email }, (error, data) => {
+    if (error) return res.send({ error: 'Erro ao buscar o usuário' });
+    if (data) return res.send({ error: 'Usuário já cadastrado' });
+
+    Users.create({ email, password }, (error, data) => {
+      if (error) return res.send({ error: `Erro ao criar o usuário ${error}` });
+
+      data.password = undefined;
+      return res.send(data);
+    });
+  });
 });
 
 module.exports = router;
